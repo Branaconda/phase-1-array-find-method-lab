e454f99fd78abd11dd0794fe7e028c087cdeaa80
Branaconda <braelli455@gmail.com>
2023-02-28 08:49:07 -0600

    completes ;ab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function yearRecord(recordLog) {
+    if (recordLog.result == "W") {
+        return true;
+    }
+    else return false;
+}
+function superbowlWin(records) {
+
+    if (records.find(yearRecord)) {
+        return records.find(yearRecord).year;
+    }
+    else return undefined;
+}
